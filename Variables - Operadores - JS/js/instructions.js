e77977e7e7c961aebf0c57c132d41d929cff8aef
Carlos Arias <43482712+carlosaariasl@users.noreply.github.com>
2020-09-04 11:36:01 -0600

    Add files via upload

---
+++ Variables - Operadores - JS/js/instructions.js
@@ -0,0 +1,13 @@
+alert("Bienvenido al sitio de suma de dos numeros");
+numero1 = prompt("Ingrese el numero1");
+numero2 = prompt("Ingrese el numero2");
+suma = parseInt(numero1) + parseInt(numero2);
+document.write("La suma del numero 1 y el numero 2 es: " , suma);
+
+
+
+alert("Bienvenido al sitio de suma de dos numeros");
+numero1 = prompt("Ingrese el numero1");
+numero2 = prompt("Ingrese el numero2");
+suma = parseInt(numero1) + parseInt(numero2);
+document.write("La suma del numero 1 y el numero 2 es: " , suma);
